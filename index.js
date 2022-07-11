8ef13a312fcd654294ab4a515175baeb4bbbaed3
gmelum <gmelum@my.com>
2022-07-11 14:12:57 +0300

    v1.0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.clamp = exports.toShort = exports.decWord = exports.timeAgo = void 0;
+exports.sliceArray = exports.clamp = exports.toShort = exports.decWord = exports.timeAgo = void 0;
 var timeAgo_1 = require("./timeAgo/timeAgo");
 Object.defineProperty(exports, "timeAgo", { enumerable: true, get: function () { return __importDefault(timeAgo_1).default; } });
 var decWord_1 = require("./decWord/decWord");
@@ -12,3 +12,5 @@ var toShort_1 = require("./toShort/toShort");
 Object.defineProperty(exports, "toShort", { enumerable: true, get: function () { return __importDefault(toShort_1).default; } });
 var clamp_1 = require("./clamp/clamp");
 Object.defineProperty(exports, "clamp", { enumerable: true, get: function () { return __importDefault(clamp_1).default; } });
+var sliceArray_1 = require("./sliceArray/sliceArray");
+Object.defineProperty(exports, "sliceArray", { enumerable: true, get: function () { return __importDefault(sliceArray_1).default; } });
